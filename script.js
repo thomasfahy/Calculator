7ecb0ab386ce118c5628bdb2108d3ac1677e0d38
thomasfahy <thomasfahy11@gmail.com>
2024-06-26 11:41:59 +0100

    feat: add clear button to UI

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function multiply(val1,val2){return val1 * val2;}
 function divide(val1,val2){return val1 / val2;}
 
 const numberBtn = document.querySelectorAll(".number");
+const funcBtn = document.querySelectorAll(".func");
 
 numberBtn.forEach(button => {
     button.addEventListener('click', () => {
@@ -11,3 +12,12 @@ numberBtn.forEach(button => {
         console.log(text);
     });
 });
+
+numberBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        let text = button.textContent;
+        console.log(text);
+    });
+});
+
+calculatorString = "";
