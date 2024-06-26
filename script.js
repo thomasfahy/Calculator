8cb8ed4249ce134f1d479af01dca886c135b92df
thomasfahy <thomasfahy11@gmail.com>
2024-06-26 11:16:02 +0100

    fix: div containers centered for digit window

---
+++ script.js
@@ -0,0 +1,4 @@
+function add(val1,val2){return val1 + val2;}
+function subtract(val1,val2){return val1 - val2;}
+function multiply(val1,val2){return val1 * val2;}
+function multiply(val1,val2){return val1 * val2;}
