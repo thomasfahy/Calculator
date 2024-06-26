7ca84005ee2d17a53f372aa51ee22ffb57fdd2f9
thomasfahy <thomasfahy11@gmail.com>
2024-06-26 11:31:41 +0100

    fix: button handlers return inputs correctly
    Due to incorrect looping of elements in js

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function subtract(val1,val2){return val1 - val2;}
 function multiply(val1,val2){return val1 * val2;}
 function divide(val1,val2){return val1 / val2;}
 
-const numberBtn = document.querySelector(".number");
+const numberBtn = document.querySelectorAll(".number");
 
-
-
-numberBtn.addEventListener('click', () => {
-    console.log("filler");
+numberBtn.forEach(button => {
+    button.addEventListener('click', () => {
+        let text = button.textContent;
+        console.log(text);
+    });
 });
