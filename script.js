9a3fecee2f4bb3449e336d9751e96464c5fa80d2
thomasfahy <thomasfahy11@gmail.com>
2024-06-26 11:21:50 +0100

    feat: add button handler for js script

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function add(val1,val2){return val1 + val2;}
 function subtract(val1,val2){return val1 - val2;}
 function multiply(val1,val2){return val1 * val2;}
-function multiply(val1,val2){return val1 * val2;}
+function divide(val1,val2){return val1 / val2;}
+
+const numberBtn = document.querySelector(".number");
+
+
+
+numberBtn.addEventListener('click', () => {
+    console.log("filler");
+});
