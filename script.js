539aac984e0e2f9f4826d03d4c934ec5daf9a026
thomasfahy <thomasfahy11@gmail.com>
2024-06-26 11:51:55 +0100

    feat: event listeners append numbers to screen

--- script.js
+++ script.js
@@ -5,19 +5,26 @@ function divide(val1,val2){return val1 / val2;}
 
 const numberBtn = document.querySelectorAll(".number");
 const funcBtn = document.querySelectorAll(".func");
+const windowText = document.querySelector(".windowText");
+const clearBtn = document.querySelector(".clear");
 
-numberBtn.forEach(button => {
+funcBtn.forEach(button => {
     button.addEventListener('click', () => {
-        let text = button.textContent;
-        console.log(text);
+        let func = button.textContent;
+        console.log(func);
     });
 });
 
 numberBtn.forEach(button => {
     button.addEventListener('click', () => {
         let text = button.textContent;
-        console.log(text);
+        calculatorString = calculatorString + text;
+        windowText.textContent = calculatorString;
     });
 });
 
+clearBtn.addEventListener('click', () => {
+    calculatorString = "";
+    windowText.textContent = "";
+})
 calculatorString = "";
